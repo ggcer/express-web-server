6a20f0ccbce29ac0997fe0b47108a0626958b988
郭刚川 <notlate@163.com>
2019-05-13 12:20:49 +0800

    init

---
+++ public/web/mobile_project/precache-manifest.6121bda6cc34fb0f9436b5ce97724b1a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "4daeb73d3927a97d4edf",
+    "url": "js/demoLazyload.9c36ce15.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "b6042862a302c6310e85",
+    "url": "css/demoStore.b3a4c3bf.css"
+  },
+  {
+    "revision": "6ce0541736fa5dc7f2a8",
+    "url": "css/chunk-vendors.28c2975a.css"
+  },
+  {
+    "revision": "b7109535e5b28c748b15",
+    "url": "js/demoForm.4b639991.js"
+  },
+  {
+    "revision": "2b41f98893dfb3fd3adf",
+    "url": "css/demoHome.bc08e031.css"
+  },
+  {
+    "revision": "2b41f98893dfb3fd3adf",
+    "url": "js/demoHome.aa9e2907.js"
+  },
+  {
+    "revision": "e00499538c0c7773a092",
+    "url": "css/demoIframe.a7057185.css"
+  },
+  {
+    "revision": "e00499538c0c7773a092",
+    "url": "js/demoIframe.4ee52e0c.js"
+  },
+  {
+    "revision": "4daeb73d3927a97d4edf",
+    "url": "css/demoLazyload.ef3c7e10.css"
+  },
+  {
+    "revision": "9d0162ba69bed18ba8df",
+    "url": "js/app.0179acd5.js"
+  },
+  {
+    "revision": "6ce0541736fa5dc7f2a8",
+    "url": "js/chunk-vendors.5595e024.js"
+  },
+  {
+    "revision": "b6042862a302c6310e85",
+    "url": "js/demoStore.0becd261.js"
+  },
+  {
+    "revision": "90ab3e934de3fc2b6b0a",
+    "url": "css/demoUi.2f640081.css"
+  },
+  {
+    "revision": "90ab3e934de3fc2b6b0a",
+    "url": "js/demoUi.7ba9d8fc.js"
+  },
+  {
+    "revision": "2183d33cbaf8704692ca",
+    "url": "js/userInfo.9ea7cfdc.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "img/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "b945175e1ed44c1d1746f93c9526a75a",
+    "url": "img/img_load_fail.b945175e.png"
+  },
+  {
+    "revision": "0fb0ce1ed8236def0ea7838c622349e2",
+    "url": "index.html"
+  },
+  {
+    "revision": "9d0162ba69bed18ba8df",
+    "url": "css/app.0b9bfb26.css"
+  }
+];
